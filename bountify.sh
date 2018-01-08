8ca77eca3954c6d29d5e2f5d7735558b56f86616
EdOverflow <edoverflow@users.noreply.github.com>
2018-01-08 16:14:53 +0100

    Rename first section.

--- bountify.sh
+++ bountify.sh
@@ -16,14 +16,15 @@ printf "${GREEN}
 
 printf "${GREEN}[+]${END} Original\\n\\n"
 cat $1
-printf "\\n${GREEN}[+]${END} Autocorrect\\n\\n"
+printf "\\n${GREEN}[+]${END} Naming convention substitution\\n\\n"
 while read line; do
     sed -E 's/Dear sir/Hi/Ig; s/TAXI DRIVER/uber/Ig; s/bounty pls|bounty please//Ig; 
-    		s/RCE/remote code execution/Ig; s/a XSS|an XSS/a cross-site scripting vulnerability/Ig; 
-    		s/XSS/cross-site scripting/Ig; s/SSRF/server-side request forgery/Ig; 
-    		s/SQLi/SQL injection/Ig; s/POC/proof of concept/Ig; s/\<i\>/I/g;
-    		s/a hacker|an attacker/an adversary/Ig; s/ \<hacker\>| \<attacker\>/ adversary/Ig; 
-    		s/hackers|attackers/adversaries/Ig; s/\<js\>/JavaScript/Ig' $1 > report.txt
+    		s/(.)RCE/\1remote code execution/Ig; s/a XSS|an XSS/a cross-site scripting vulnerability/Ig; 
+    		s/(.)XSS/\1cross-site scripting/Ig; s/(.)SSRF/\1server-side request forgery/Ig; 
+    		s/SQLi/SQL injection/Ig; s/(.)POC/\1proof of concept/Ig; s/\<i\>/I/g;
+    		s/a hacker|an attacker/an adversary/Ig; s/(.)\<hacker\>|(.)\<attacker\>/\1adversary/Ig; 
+    		s/(.)hackers|(.)attackers/\1adversaries/Ig; s/\<js\>/JavaScript/Ig;
+    		s/(.)IDOR/\1insecure Direct Object Reference/g' $1 > report.txt
 done < $1
 cat report.txt
 printf "\\n"
